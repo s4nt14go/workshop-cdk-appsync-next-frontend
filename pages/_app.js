01344f5d99f73987caf6ed8f887613feddb52e5b
s4nt14go <santiagopergamo@gmail.com>
2020-12-25 19:31:16 -0300

    feat: add my posts page
    Authenticated query postsByUsername

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,28 @@ import '../styles/globals.css'
 import styles from '../styles/Home.module.css'
 import '../configureAmplify'
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { Auth, Hub } from 'aws-amplify'
 
 function MyApp({ Component, pageProps }) {
+  const [signedInUser, setSignedInUser] = useState(false)
+  useEffect(() => {
+    authListener()
+  })
+  async function authListener() {
+    Hub.listen('auth', (data) => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return setSignedInUser(true)
+        case 'signOut':
+          return setSignedInUser(false)
+      }
+    })
+    try {
+      await Auth.currentAuthenticatedUser()
+      setSignedInUser(true)
+    } catch (err) {}
+  }
   return (
     <div>
       <nav style={navStyle}>
@@ -16,6 +36,13 @@ function MyApp({ Component, pageProps }) {
         <Link href="/profile">
           <span style={linkStyle}>Profile</span>
         </Link>
+        {
+          signedInUser && (
+            <Link href="/my-posts">
+              <span style={linkStyle}>My Posts</span>
+            </Link>
+          )
+        }
       </nav>
       <div style={bodyStyle}>
         <Component {...pageProps} />
