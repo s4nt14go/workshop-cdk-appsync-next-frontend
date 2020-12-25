8f309c8c734f6afbc103302498e98c129ccf565c
s4nt14go <santiagopergamo@gmail.com>
2020-12-25 09:28:14 -0300

    feat: configure Amplify

---
+++ aws-exports.js
@@ -0,0 +1,14 @@
+import { NextBackendStack } from './cdk-exports.json'
+
+const config = {
+  // Cognito
+  aws_project_region: NextBackendStack.ProjectRegion,
+  aws_user_pools_id: NextBackendStack.UserPoolId,
+  aws_user_pools_web_client_id: NextBackendStack.UserPoolClientId,
+  // AppSync
+  aws_appsync_graphqlEndpoint: NextBackendStack.GraphQLAPIURL,
+  aws_appsync_apiKey: NextBackendStack.AppSyncAPIKey,
+  aws_appsync_authenticationType: "API_KEY"
+}
+
+export default config
